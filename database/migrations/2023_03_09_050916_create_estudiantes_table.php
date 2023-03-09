fb4cf085151127ac7d7d501f64b19165cead3a58
JohannTabares <al02972975@tecmilenio.mx>
2023-03-08 23:20:54 -0600

    Agregando datos

--- database/migrations/2023_03_09_050916_create_estudiantes_table.php
+++ database/migrations/2023_03_09_050916_create_estudiantes_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('estudiantes', function (Blueprint $table) {
             $table->id();
+            $table->string('NombreGrupo');
+            $table->string('NombreCurso');
+
             $table->timestamps();
         });
     }
